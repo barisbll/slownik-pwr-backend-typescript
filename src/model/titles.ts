31bc7ebdd88d8d278988920b0a4f17c72c97c8f4
Baris Balli <barisbll.dev@gmail.com>
2022-05-18 19:46:09 +0200

    text searches are complete

--- src/model/titles.ts
+++ src/model/titles.ts
@@ -11,12 +11,14 @@ interface TitleI {
 }
 
 const title = new Schema<TitleI>({
-  name: { type: String, required: true },
+  name: { type: String, text: true, required: true },
   counter: { type: Number, required: true },
   dayCounter: { type: Number, required: true },
   weekCounter: { type: Number, required: true },
   monthCounter: { type: Number, required: true },
   posts: { type: [Types.ObjectId], required: true, ref: "Post" },
 });
+
+title.index({ name: "text" });
 
 export const Title = mongoose.model("Title", title);
