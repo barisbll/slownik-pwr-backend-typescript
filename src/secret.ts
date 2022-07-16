7dd915a2627db4d573142c33596d6c77a7120271
Baris Balli <barisbll.dev@gmail.com>
2022-07-16 13:05:13 +0200

    dev url is used

--- src/secret.ts
+++ src/secret.ts
@@ -2,5 +2,5 @@ const productionURL = `mongodb+srv://${process.env.PROD_MONGODB_USERNAME}:${proc
 const developmentURL = `mongodb+srv://${process.env.DEV_MONGODB_USERNAME}:${process.env.DEV_MONGODB_PASSWORD}@slownik-pwr.gqzot.mongodb.net/?retryWrites=true&w=majority`;
 
 export default {
-  mongodbSecret: productionURL,
+  mongodbSecret: developmentURL,
 };
