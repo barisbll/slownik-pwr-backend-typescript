637fa101714bdcb013503bad7721f34503d1b5e6
Baris Balli <barisbll.dev@gmail.com>
2022-07-16 13:25:10 +0200

    production url changed

--- src/secret.ts
+++ src/secret.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const productionURL = `mongodb+srv://${process.env.PROD_MONGODB_USERNAME}:${process.env.PROD_MONGODB_PASSWORD}@slownik-pwr-cluster.dvcq2bd.mongodb.net/${process.env.PROD_MONGODB_DB_NAME}?retryWrites=true&w=majority`;
 const developmentURL = `mongodb+srv://${process.env.DEV_MONGODB_USERNAME}:${process.env.DEV_MONGODB_PASSWORD}@slownik-pwr.gqzot.mongodb.net/?retryWrites=true&w=majority`;
+const productionURL = `mongodb+srv://${process.env.PROD_MONGODB_USERNAME}:${process.env.PROD_MONGODB_PASSWORD}@slownik-pwr-cluster.dvcq2bd.mongodb.net/${process.env.PROD_MONGODB_DB_NAME}?retryWrites=true&w=majority`;
 
 export default {
-  mongodbSecret: developmentURL,
+  mongodbSecret: productionURL,
 };
