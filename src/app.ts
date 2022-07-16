e21d57f6657dbb674376771e451cd3802776d81f
Baris Balli <barisbll.dev@gmail.com>
2022-07-16 13:17:42 +0200

    helmet added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-redeclare
-import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
+import "dotenv/config";
 
 import { json } from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
+import helmet from "helmet";
 
 import secret from "./secret";
 import postRoutes from "./routes/posts";
@@ -16,6 +17,7 @@ const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(helmet());
 
 // Routes
 app.use("/posts", postRoutes);
